1b82143483c8ea77184ad6bbe142cbcc285a8ced
Erick <1722110095@gmail.com>
2023-11-01 10:29:20 -0600

    a

--- static/js/get_for_email.js
+++ static/js/get_for_email.js
@@ -12,7 +12,7 @@ function getForEmail() {
         console.log("json: " + json);
         console.log("status_code: " + request.status);
 
-        if (json.length =! null) {
+        if (response != null) {
             var tbody_contactos = document.getElementById("tbody_contactos");
             var tr = document.createElement("tr");
             var td_email = document.createElement("td");
